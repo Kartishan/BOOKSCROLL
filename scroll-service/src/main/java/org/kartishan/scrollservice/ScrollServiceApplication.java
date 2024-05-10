cf979891172eb8f855c987563982a0f0e83f6b35
Kartishan <iiiggoorr02@gmail.com>
2024-05-11 00:20:18 +0300

    init scroll-service

---
+++ scroll-service/src/main/java/org/kartishan/scrollservice/ScrollServiceApplication.java
@@ -0,0 +1,13 @@
+package org.kartishan.scrollservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrollServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrollServiceApplication.class, args);
+    }
+
+}
---
+++ scroll-service/src/test/java/org/kartishan/scrollservice/ScrollServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kartishan.scrollservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrollServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
