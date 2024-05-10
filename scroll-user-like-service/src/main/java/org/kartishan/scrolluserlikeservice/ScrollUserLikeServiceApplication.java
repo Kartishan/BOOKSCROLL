b226d13f11ecc24f43ff8b30aa0cc15a581f249d
Kartishan <iiiggoorr02@gmail.com>
2024-05-11 01:31:59 +0300

    init user scroll like service

---
+++ scroll-user-like-service/src/main/java/org/kartishan/scrolluserlikeservice/ScrollUserLikeServiceApplication.java
@@ -0,0 +1,13 @@
+package org.kartishan.scrolluserlikeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrollUserLikeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrollUserLikeServiceApplication.class, args);
+    }
+
+}
