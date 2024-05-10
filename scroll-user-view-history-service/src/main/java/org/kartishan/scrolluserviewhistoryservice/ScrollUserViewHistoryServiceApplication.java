d7514cab69596e667582b239cc799d2cd6cf52f7
Kartishan <iiiggoorr02@gmail.com>
2024-05-11 01:11:35 +0300

    init scroll-user-view-history-service

---
+++ scroll-user-view-history-service/src/main/java/org/kartishan/scrolluserviewhistoryservice/ScrollUserViewHistoryServiceApplication.java
@@ -0,0 +1,13 @@
+package org.kartishan.scrolluserviewhistoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrollUserViewHistoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrollUserViewHistoryServiceApplication.class, args);
+    }
+
+}
---
+++ scroll-user-view-history-service/src/test/java/org/kartishan/scrolluserviewhistoryservice/ScrollUserViewHistoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kartishan.scrolluserviewhistoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrollUserViewHistoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
