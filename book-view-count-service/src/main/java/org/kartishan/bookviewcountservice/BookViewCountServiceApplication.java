120b24e1db1523b0f6d8c6eeb8dd9d7e6315be86
Kartishan <iiiggoorr02@gmail.com>
2024-05-10 14:51:19 +0300

    init book view count service

---
+++ book-view-count-service/src/main/java/org/kartishan/bookviewcountservice/BookViewCountServiceApplication.java
@@ -0,0 +1,13 @@
+package org.kartishan.bookviewcountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookViewCountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookViewCountServiceApplication.class, args);
+    }
+
+}
---
+++ book-view-count-service/src/test/java/org/kartishan/bookviewcountservice/BookViewCountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kartishan.bookviewcountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookViewCountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
