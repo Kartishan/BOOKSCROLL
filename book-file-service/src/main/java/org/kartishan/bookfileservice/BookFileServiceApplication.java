58028481d8faf7ca992cfd6329c5aec954f2b814
Kartishan <iiiggoorr02@gmail.com>
2024-05-10 16:07:53 +0300

    init book file service

---
+++ book-file-service/src/main/java/org/kartishan/bookfileservice/BookFileServiceApplication.java
@@ -0,0 +1,13 @@
+package org.kartishan.bookfileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookFileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookFileServiceApplication.class, args);
+    }
+
+}
---
+++ book-file-service/src/test/java/org/kartishan/bookfileservice/BookFileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kartishan.bookfileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookFileServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
