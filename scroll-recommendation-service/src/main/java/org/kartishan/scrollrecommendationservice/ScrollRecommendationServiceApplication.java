23bcacc35801a4cd5902e0df3e3af2cbac62b924
Kartishan <iiiggoorr02@gmail.com>
2024-05-11 01:47:28 +0300

    init scroll recommendation service

---
+++ scroll-recommendation-service/src/main/java/org/kartishan/scrollrecommendationservice/ScrollRecommendationServiceApplication.java
@@ -0,0 +1,13 @@
+package org.kartishan.scrollrecommendationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrollRecommendationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrollRecommendationServiceApplication.class, args);
+    }
+
+}
---
+++ scroll-recommendation-service/src/test/java/org/kartishan/scrollrecommendationservice/ScrollRecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kartishan.scrollrecommendationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrollRecommendationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
