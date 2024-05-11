917c5cbf3aa768645e587587fd9c684aee1ed9a3
Kartishan <iiiggoorr02@gmail.com>
2024-05-11 19:58:58 +0300

    add scroll dto

---
+++ scroll-service/src/main/java/org/kartishan/scrollservice/model/dto/ScrollDTO.java
@@ -0,0 +1,17 @@
+package org.kartishan.scrollservice.model.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Builder
+public class ScrollDTO {
+    private UUID id;
+    private String name;
+    private UUID bookId;
+    private String bookName;
+    private UUID userId;
+    private String username;
+}
