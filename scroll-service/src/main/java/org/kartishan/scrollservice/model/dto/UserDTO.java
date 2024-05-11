b8383018c1e90fc61f4dbf8f8078549263e16933
Kartishan <iiiggoorr02@gmail.com>
2024-05-11 19:59:52 +0300

    add UserDTOto

---
+++ scroll-service/src/main/java/org/kartishan/scrollservice/model/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package org.kartishan.scrollservice.model.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Builder
+public class UserDTO {
+    private UUID id;
+    private String username;
+    private String email;
+}
