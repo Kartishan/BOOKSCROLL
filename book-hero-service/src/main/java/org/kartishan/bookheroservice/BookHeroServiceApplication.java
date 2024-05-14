1d30419ea93eda24959b4768e50b644ee82807d5
Kartishan <iiiggoorr02@gmail.com>
2024-05-14 14:58:20 +0300

    init heroes service

---
+++ book-hero-service/src/main/java/org/kartishan/bookheroservice/BookHeroServiceApplication.java
@@ -0,0 +1,13 @@
+package org.kartishan.bookheroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookHeroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookHeroServiceApplication.class, args);
+    }
+
+}
