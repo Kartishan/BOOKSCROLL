fdca414daa3cdf6237b87123ae7f45c5051bc6ad
Kartishan <iiiggoorr02@gmail.com>
2024-05-14 21:42:08 +0300

    init book-chapter

---
+++ book-chapter-service/src/main/java/org/kartishan/bookchapterservice/BookChapterServiceApplication.java
@@ -0,0 +1,13 @@
+package org.kartishan.bookchapterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookChapterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookChapterServiceApplication.class, args);
+    }
+
+}
---
+++ book-chapter-service/src/test/java/org/kartishan/bookchapterservice/BookChapterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kartishan.bookchapterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookChapterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
